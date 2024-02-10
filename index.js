a2200816c4f50f6ccbb3baa711a5c077517d831d
CAREFREE-VILLAGER <as314202@gmail.com>
2024-02-11 02:37:49 +0530

    minor changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const homeRoutes = require('./routes/homeRoutes');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -14,7 +15,7 @@ const app = express();
 
 
 app.use(express.json());
-
+app.use(cors());
 
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
