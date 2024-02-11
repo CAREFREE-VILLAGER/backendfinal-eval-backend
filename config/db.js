06ae22ae9bf59a5d259b5f33d11af64caac0a0eb
CAREFREE-VILLAGER <as314202@gmail.com>
2024-02-11 12:15:41 +0530

    changed url to uri once  more

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config(); 
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv').config(); 
 
-const mongoUrl = process.env.MONGODB_URI;
-const config = {
-    connectTimeoutMS: 30000,
-    socketTimeoutMS: 30000,
-    useUnifiedTopology: true
-  }
+// const mongoUrl = process.env.MONGODB_URI;
+// const config = {
+//     connectTimeoutMS: 30000,
+//     socketTimeoutMS: 30000,
+//     useUnifiedTopology: true
+//   }
 
 //   const connectDB = async ()=>{
 //     try{
@@ -17,8 +17,8 @@ const config = {
 //         console.log("MongoDB connection error:",err);
 //     }
 //   };
-mongoose.connect(mongoUrl,config).then(() => {
-    console.log('MongoDB connected');
-}).catch(err => console.error('MongoDB connection error:', err));
+// mongoose.connect(mongoUrl,config).then(() => {
+//     console.log('MongoDB connected');
+// }).catch(err => console.error('MongoDB connection error:', err));
 
-module.exports= mongoose.connection;
+// module.exports= mongoose.connection;
--- index.js
+++ index.js
@@ -41,3 +41,30 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+//db.js
+
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config(); 
+
+const mongoUrl = process.env.MONGODB_URL;
+const config = {
+    connectTimeoutMS: 30000,
+    socketTimeoutMS: 30000,
+    useUnifiedTopology: true
+  }
+
+//   const connectDB = async ()=>{
+//     try{
+//         await mongoose.connect(mongoUrl,config);
+//         console.log('MongoDB connected');
+//     }catch(err){
+//         console.log("MongoDB connection error:",err);
+//     }
+//   };
+mongoose.connect(mongoUrl,config).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => console.error('MongoDB connection error:', err));
+
+module.exports= mongoose.connection;
