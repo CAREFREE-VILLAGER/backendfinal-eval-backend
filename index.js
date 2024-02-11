d7474502f207f61551c6fd8f66e9492e4bd94f20
CAREFREE-VILLAGER <as314202@gmail.com>
2024-02-11 10:25:05 +0530

    changed cors policy

--- index.js
+++ index.js
@@ -16,10 +16,26 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.options('*', cors())
+
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 app.use('/', homeRoutes);
+const corsOptions = {
+  origin: 'http://localhost:3000/',
+  optionsSuccessStatus: 200,
+  credentials: true
+  
+}
+
+app.use(cors(corsOptions));
+
+
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
