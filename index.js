77fc5b75913faefd353b6027ce06806b9d902a11
CAREFREE-VILLAGER <as314202@gmail.com>
2024-02-11 10:08:28 +0530

    modified index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.options('*', cors())
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 app.use('/', homeRoutes);
