d27f083160ba95c38fc670efffec85a0cb9fa7d2
CAREFREE-VILLAGER <as314202@gmail.com>
2024-02-11 13:02:01 +0530

    trying final

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config(); 
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv').config(); 
 
-const mongoUrl = process.env.MONGODB_URI;
-console.log(process.env.MONGODB_URI);
-const config = {
-    connectTimeoutMS: 30000,
-    socketTimeoutMS: 30000,
-    useUnifiedTopology: true
-  }
+// const mongoUrl = process.env.MONGODB_URI;
+// console.log(process.env.MONGODB_URI);
+// const config = {
+//     connectTimeoutMS: 30000,
+//     socketTimeoutMS: 30000,
+//     useUnifiedTopology: true
+//   }
 
 //   const connectDB = async ()=>{
 //     try{
@@ -18,8 +18,8 @@ const config = {
 //         console.log("MongoDB connection error:",err);
 //     }
 //   };
-mongoose.connect(mongoUrl,config).then(() => {
-    console.log('MongoDB connected');
-}).catch(err => console.error('MongoDB connection error:', err));
+// mongoose.connect(mongoUrl,config).then(() => {
+//     console.log('MongoDB connected');
+// }).catch(err => console.error('MongoDB connection error:', err));
 
-module.exports= mongoose.connection;
+// module.exports= mongoose.connection;
